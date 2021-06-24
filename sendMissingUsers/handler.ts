167f3d89e4c80b7e0a0f3e7a5fcd77299cf8b332
fabriziopapi <fabrizio.papi@pagopa.it>
2021-06-24 17:33:44 +0200

    add sendMissingUsers

---
+++ sendMissingUsers/handler.ts
@@ -0,0 +1,120 @@
+/* eslint-disable max-params */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createWriteStream } from "fs";
+import { QueueService, TableQuery, TableUtilities } from "azure-storage";
+
+import { toError } from "fp-ts/lib/Either";
+import * as te from "fp-ts/lib/TaskEither";
+import * as a from "fp-ts/lib/Array";
+import * as t from "fp-ts/lib/Task";
+import {
+  getInsertBulkFiscalCodes,
+  getPagedQuery,
+  iterateOnPages,
+} from "../utils/table_storage";
+import { IConfig } from "../utils/config";
+
+const TO_SEND = "TO_SEND";
+const ERROR = "ERROR";
+const SENDED = "SENDED";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const getStatusCodeItem = (status: string) => ({
+  Status: TableUtilities.entityGenerator.String(status),
+});
+
+export type Logger = ReturnType<typeof createLogger>;
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const createLogger = () => {
+  const logFileName = `${process.cwd()}/log.${Date.now()}.csv`;
+  const logStream = createWriteStream(logFileName, {
+    flags: "a",
+  });
+  // header
+  logStream.write(`log level,\ttext\n`);
+
+  return {
+    finalize: (): void => logStream.close(),
+    logFailure: (info: string, error: unknown): boolean =>
+      logStream.write(`failure,\t${info}: ${error}\n`),
+    logInfo: (info: string): boolean => logStream.write(`info,${info}\t\n`),
+  };
+};
+
+const sendPage = (
+  queueService: QueueService,
+  NOTIFY_USER_QUEUE_NAME: string,
+  updateBulkFiscalCodesWithMessage: ReturnType<typeof getInsertBulkFiscalCodes>,
+  page: ReadonlyArray<
+    Readonly<{
+      readonly RowKey: Readonly<{
+        readonly _: string;
+      }>;
+    }>
+  >
+): t.Task<ReadonlyArray<string>> =>
+  a.array.sequence(t.task)(
+    page
+      .map((e) => e.RowKey._)
+      .map((fiscalCode) =>
+        te
+          .taskify<Error, QueueService.QueueMessageResult>((cb) =>
+            queueService.createMessage(
+              NOTIFY_USER_QUEUE_NAME,
+              Buffer.from(JSON.stringify({ fiscal_code: fiscalCode })).toString(
+                "base64"
+              ),
+              cb
+            )
+          )()
+          .fold(
+            (_) => ({ fiscalCode, status: ERROR }),
+            (_) => ({ fiscalCode, status: SENDED })
+          )
+          .chain((fc) =>
+            updateBulkFiscalCodesWithMessage(
+              [fc.fiscalCode],
+              getStatusCodeItem(fc.status)
+            ).fold(
+              (l) => fiscalCode,
+              (r) => fiscalCode
+            )
+          )
+      )
+  );
+
+/**
+ *
+ */
+export const getHandler =
+  (
+    getProfilesWithoutMessages: ReturnType<typeof getPagedQuery>,
+    queueService: QueueService,
+    updateBulkFiscalCodesWithMessage: ReturnType<
+      typeof getInsertBulkFiscalCodes
+    >,
+    { NOTIFY_USER_QUEUE_NAME }: IConfig
+  ) =>
+  async (): Promise<void> => {
+    await te.taskEither
+      .of<Error, ReturnType<typeof getProfilesWithoutMessages>>(
+        getProfilesWithoutMessages(
+          new TableQuery().select().where("Status eq ?", TO_SEND)
+        )
+      )
+      .chain((allUsersQuery) =>
+        te.tryCatch(async () => {
+          for await (const page of iterateOnPages(allUsersQuery)) {
+            // eslint-disable-next-line no-console
+            await sendPage(
+              queueService,
+              NOTIFY_USER_QUEUE_NAME,
+              updateBulkFiscalCodesWithMessage,
+              page
+            ).run();
+          }
+        }, toError)
+      )
+      .run();
+  };
---
+++ sendMissingUsers/index.ts
@@ -0,0 +1,69 @@
+import {
+  createTableService,
+  ErrorOrResult,
+  QueueService,
+  TableQuery,
+  TableService,
+} from "azure-storage";
+import { getConfigOrThrow } from "../utils/config";
+import { getUdateBulkFiscalCodes } from "../utils/table_storage";
+import { getPagedQuery } from "../utils/table_storage";
+import { getHandler } from "./handler";
+
+export const MESSAGES_COLLECTION_NAME = "messages";
+export const PROFILES_COLLECTION_NAME = "profiles";
+
+const config = getConfigOrThrow();
+
+const queueService = new QueueService(config.NOTIFY_USER_QUEUE_CONNECTION);
+
+const tableService = createTableService(
+  config.SCRIPT_STORAGE_CONNECTION_STRING
+);
+
+export const createQueryForAllProfiles =
+  <T>(ts: TableService) =>
+  (
+    table: string,
+    tableQuery: TableQuery,
+    currentToken: TableService.TableContinuationToken,
+    callback: ErrorOrResult<TableService.QueryEntitiesResult<T>>
+  ): void =>
+    ts.queryEntities(table, tableQuery, currentToken, callback);
+
+tableService.createTableIfNotExists(
+  config.PROFILE_WITH_MESSAGE_TABLE_NAME,
+  (err1, _res1) => {
+    if (err1) {
+      // eslint-disable-next-line no-console
+      throw Error("Table PROFILE_WITH_MESSAGE_TABLE_NAME not crated");
+    }
+
+    tableService.createTableIfNotExists(
+      config.PROFILE_TABLE_NAME,
+      (err, _res) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          throw Error("Table PROFILE_TABLE_NAME not crated");
+        }
+
+        getHandler(
+          getPagedQuery(tableService, config.PROFILE_TABLE_NAME),
+          queueService,
+          getUdateBulkFiscalCodes(tableService, config.PROFILE_TABLE_NAME),
+          config
+        )()
+          .then((c) => {
+            // eslint-disable-next-line no-console
+            console.log(`Processed ${c} profiles`);
+            process.exit(0);
+          })
+          .catch((c) => {
+            // eslint-disable-next-line no-console
+            console.error(`Failed to process profiles`, c);
+            process.exit(1);
+          });
+      }
+    );
+  }
+);
